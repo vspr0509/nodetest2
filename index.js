38535ad25305a00493f48c7255fb9d2ad73b34c6
vspr0509 <121675630+vspr0509@users.noreply.github.com>
2025-01-05 13:51:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ http
     const statusCode = file.found ? 200 : 404;
     const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
     const headers = { "Content-Type": mimeType };
-    if(["jpg", "png", "gif", "svg"].includes(file.ext)) {
+    
       // headers["Access-Control-Allow-Origin"] = "https://developer.mozilla.org";
       // headers["Access-Control-Allow-Origin"] = "http://localhost:8000";
       headers["Access-Control-Allow-Origin"] = "https://nodetest-la3e.onrender.com";
  headers["Cross-Origin-Resource-Policy"] = "same-origin";
       headers["Set-Cookie"] = "mycookie=test; Secure; SameSite=None";
       // headers["Access-Control-Allow-Origin"] = "https://localhost:8000";
-    }
+    
     res.writeHead(statusCode, headers);
     file.stream.pipe(res);
     console.log(`${req.method} ${req.url} ${statusCode}`);
