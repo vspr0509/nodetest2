cc40c7d3e787b95cc42ebd7ebf6c18563c317eaf
vspr0509 <121675630+vspr0509@users.noreply.github.com>
2025-04-08 19:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ http
     const statusCode = file.found ? 200 : 404;
     const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
     const headers = { "Content-Type": mimeType };
-    
+    if(mimeType.indexOf("img") > -1) {
+      headers["content-disposition"] = "attachment";
+}
       // headers["Access-Control-Allow-Origin"] = "https://developer.mozilla.org";
       // headers["Access-Control-Allow-Origin"] = "http://localhost:8000";
       headers["Access-Control-Allow-Origin"] = "https://nodetest-la3e.onrender.com";
